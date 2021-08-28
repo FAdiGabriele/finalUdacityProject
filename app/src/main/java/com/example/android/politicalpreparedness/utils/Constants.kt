4cfb388476c2510c4d73947245e4f13f45aa3cd6
Fabio A. di Gabriele <fabioa.digabriele@gmail.com>
2021-08-28 16:10:05 +0200

    set location permissions and constants

---
+++ app/src/main/java/com/example/android/politicalpreparedness/utils/Constants.kt
@@ -0,0 +1,15 @@
+package com.example.android.politicalpreparedness.utils
+
+object Constants {
+
+    //Log Tags
+    const val TAG = "ElectionLog"
+    const val LOCATION_TAG = "LocationElectionLog"
+    const val NETWORK_TAG = "NetworkElectionLog"
+
+    //location constants
+    const val REQUEST_FOREGROUND_PERMISSIONS_REQUEST_CODE = 34
+//    const val REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSIONS_REQUEST_CODE = 35
+    const val REQUEST_TURN_DEVICE_LOCATION_ON = 29
+    const val LOCATION_PERMISSION_INDEX = 0
+}
